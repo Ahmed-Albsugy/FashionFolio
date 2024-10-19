2d1e277f83d4af3a61fb3ce9ae23086e6128d7cd
Ahmed <ahmed.albsugy@gmail.com>
2024-10-19 12:32:53 +0300

    best selling styling

--- src/app/home/product-slider/product-slider.component.ts
+++ src/app/home/product-slider/product-slider.component.ts
@@ -25,45 +25,7 @@ import { NgZone } from '@angular/core';
   styleUrls: ['./product-slider.component.css'],
 })
 export class ProductSliderComponent implements OnInit {
-  products$: Observable<Product[]> = of([]);
-  slideConfig = {
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    dots: true,
-    infinite: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
-  visibleProducts$: Observable<Product[]> = of([]);
-  totalProducts$: Observable<number> = of(0);
-  currentIndex = 0;
-  itemsPerSlide = 4;
-  private resizeSubscription?: Subscription;
-  private autoSlideSubscription?: Subscription;
-
-  // products: Product[] = [];
+  products: Product[] = [];
 
   constructor(
     private snackBar: MatSnackBar,
@@ -74,125 +36,10 @@ export class ProductSliderComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts();
-    // this.startAutoSlide();
-    // this.products$ = this.productService.getProducts();
-    // this.totalProducts$ = this.products$.pipe(
-    //   map((products: Product[]) => products.length)
-    // );
-    // this.updateVisibleProducts();
-
-    // this.adjustItemsPerSlide();
-    // this.resizeSubscription = fromEvent(window, 'resize')
-    //   .pipe(debounceTime(250))
-    //   .subscribe(() => {
-    //     this.adjustItemsPerSlide();
-    //     this.updateVisibleProducts();
-    //   });
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
   }
-  // ngOnDestroy(): void {
-  //   this.stopAutoSlide();
-  //   if (this.resizeSubscription) {
-  //     this.resizeSubscription.unsubscribe();
-  //   }
-  // }
-
-  adjustItemsPerSlide(): void {
-    const width = window.innerWidth;
-    if (width < 768) {
-      this.itemsPerSlide = 1;
-    } else if (width < 1024) {
-      this.itemsPerSlide = 2;
-    } else if (width < 1200) {
-      this.itemsPerSlide = 3;
-    } else {
-      this.itemsPerSlide = 4;
-    }
-    this.currentIndex = 0;
-  }
-
-  // handlePrev() {
-  //   this.currentIndex =
-  //     (this.currentIndex - 1 + this.products$.length) % this.products.length;
-  // }
-
-  // handleNext() {
-  //   this.currentIndex = (this.currentIndex + 1) % this.products.length;
-  // }
-  // adjustItemsPerSlide() {
-  //   if (window.innerWidth < 768) {
-  //     this.itemsPerSlide = 1;
-  //   } else if (window.innerWidth < 1024) {
-  //     this.itemsPerSlide = 2;
-  //   } else if (window.innerWidth < 1200) {
-  //     this.itemsPerSlide = 3;
-  //   } else {
-  //     this.itemsPerSlide = 4;
-  //   }
-  //   this.currentIndex = 0;
-  // }
-
-  // nextSlide() {
-  //   this.currentIndex =
-  //     (this.currentIndex + 1) % (this.products.length - this.itemsPerSlide + 1);
-  // }
-
-  // prevSlide() {
-  //   this.currentIndex =
-  //     (this.currentIndex - 1 + this.products.length) %
-  //     (this.products.length - this.itemsPerSlide + 1);
-  // }
-
-  nextSlide(): void {
-    this.products$
-      .pipe(map((products) => products.length))
-      .subscribe((length) => {
-        this.currentIndex = Math.min(
-          this.currentIndex + 1,
-          length - this.itemsPerSlide
-        );
-        this.updateVisibleProducts();
-      });
-  }
-
-  prevSlide(): void {
-    this.currentIndex = Math.max(0, this.currentIndex - 1);
-    this.updateVisibleProducts();
-  }
-
-  // nextSlide() {
-  //   this.products$
-  //     .pipe(map((products) => products.length))
-  //     .subscribe((length) => {
-  //       this.currentIndex = Math.min(
-  //         this.currentIndex + 1,
-  //         length - this.itemsPerSlide
-  //       );
-  //       this.updateVisibleProducts();
-  //     });
-  // }
-
-  // prevSlide() {
-  //   this.currentIndex = Math.max(0, this.currentIndex - 1);
-  //   this.updateVisibleProducts();
-  // }
-
-  updateVisibleProducts(): void {
-    this.visibleProducts$ = this.products$.pipe(
-      map((products) =>
-        products.slice(
-          this.currentIndex,
-          this.currentIndex + this.itemsPerSlide
-        )
-      )
-    );
-  }
-  // visibleProducts() {
-  //   return this.products.slice(
-  //     this.currentIndex,
-  //     this.currentIndex + this.itemsPerSlide
-  //   );
-  // }
 
   addToCart(product: Product): void {
     this.snackBar.open(`${product.name} added to cart`, 'Close', {
